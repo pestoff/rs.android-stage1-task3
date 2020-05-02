f944a4b7ab1913b63ba424a295a9fb48de14bb2f
Ivan Pestov <pestoff95@gmail.com>
2020-05-02 19:13:46 +0500

    feat: add first and second task

--- src/main/kotlin/subtask1/PolynomialConverter.kt
+++ src/main/kotlin/subtask1/PolynomialConverter.kt
@@ -1,9 +1,32 @@
 package subtask1
 
+import kotlin.math.absoluteValue
+
 class PolynomialConverter {
 
-    // TODO: Complete the following function
     fun convertToStringFrom(numbers: Array<Int>): String? {
-        throw NotImplementedError("Not implemented")
+
+        var result = ""
+
+        for ((index, element) in numbers.withIndex()) {
+
+            if (element == 0) continue
+
+            if (index == 0)
+                result += "${element}x^${numbers.size - 1}"
+            else {
+                val symbol = if (element >= 0) "+" else "-"
+                val number = if (element.absoluteValue != 1) "${element.absoluteValue}" else ""
+                val degree = when {
+                    numbers.size - (index + 1) == 0 -> ""
+                    numbers.size - (index + 1) == 1 -> "x"
+                    else -> "x^${numbers.size - (index + 1)}"
+                }
+
+                result += " $symbol $number$degree"
+            }
+        }
+
+        return if (result == "") null else result
     }
 }
--- src/main/kotlin/subtask2/Combinator.kt
+++ src/main/kotlin/subtask2/Combinator.kt
@@ -4,6 +4,38 @@ class Combinator {
 
     // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+        var result = array.first()
+        var n: Long = array.last().toLong()
+
+
+        var m = n.factorial() / result
+
+        for (i in 0..m) {
+            val x = (n-i).factorial() * i.factorial()
+            if (x == m)
+                return i.toInt()
+        }
+
+        return null
     }
+}
+
+fun Int.factorial() : Int {
+    var result = 1
+
+    for (i in 1..this) {
+        result *= i
+    }
+
+    return result
+}
+
+fun Long.factorial() : Long {
+    var result: Long = 1
+
+    for (i in 1..this) {
+        result *= i
+    }
+
+    return result
 }
