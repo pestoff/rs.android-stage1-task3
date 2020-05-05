f81bad517f470088b69c8b2677f80b8c3f00087b
Ivan Pestov <pestoff95@gmail.com>
2020-05-05 17:27:03 +0500

    feat: add task4

--- src/main/kotlin/subtask4/SquareDecomposer.kt
+++ src/main/kotlin/subtask4/SquareDecomposer.kt
@@ -1,9 +1,68 @@
 package subtask4
 
+import kotlin.math.sqrt
+
 class SquareDecomposer {
 
     // TODO: Complete the following function
     fun decomposeNumber(number: Int): Array<Int>? {
-        throw NotImplementedError("Not implemented")
+
+        if (number < 4) return null
+
+        val differnce = (number - (number - 1)) * (number + (number -1))
+        val output = langrange(differnce)
+
+        output.add(output.size, number - 1)
+
+        return output.toTypedArray()
     }
+
+
+    fun langrange(input: Int): MutableList<Int> {
+        val output = mutableListOf<Int>()
+
+        val sqrtInput = input.sqrt()
+
+        for (i in sqrtInput downTo 1) {
+            if (i.sqr() == input) {
+                output.add(i)
+                return output
+            }
+
+            for (j in i-1 downTo 1) {
+                if (i.sqr() + j.sqr() == input) {
+                    output.addAll(arrayOf(j, i))
+                    return output
+                }
+
+                for (k in j-1 downTo 1) {
+                    if (i.sqr() + j.sqr() + k.sqr() == input) {
+                        output.addAll(arrayOf(j, i, k))
+                        return output
+                    }
+
+                    for (m in k-1 downTo 1) {
+                        val temp = i.sqr() + j.sqr() + k.sqr() + m.sqr()
+                        if (temp == input) {
+                            val array = arrayOf(m, k, j, i)
+                            output.addAll(array)
+                            return output
+
+                        }
+                    }
+                }
+            }
+        }
+
+        return output
+    }
+
+    fun Int.sqr() = this * this
+
+    fun Int.sqrt() = sqrt(this.toDouble()).toInt()
+
+
+
+
+
 }
